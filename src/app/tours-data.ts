1296caa894975d25776d04d2bdd25c8808ac3e63
Ivan Lantinov <valeryiivanovi4@gmail.com>
2026-04-18 23:52:07 +0200

    jkdnbfghdsn

--- src/app/tours-data.ts
+++ src/app/tours-data.ts
@@ -5,7 +5,7 @@ export const CONTACTS = {
 export const LANGUAGES: any = {
   en: { 
     name: "EN", 
-    brand: "AhVan Tour",
+    brand: "Primal Tour",
     heroTitle: "Hurghada is Waiting", 
     heroSub: "Best excursions from direct organizers",
     toursTitle: "Popular Excursions", 
